373406fea355425aaa577a021d13594dccd5e09d
madhupriya141 <priyavas141madhu@gmail.com>
2020-03-26 17:10:25 +0530

    Done

---
+++ assignment1/src/com/assignment/task.java
@@ -0,0 +1,13 @@
+package com.assignment;
+
+import java.util.Scanner;
+
+public class task {
+public static void main(String[] args) {
+	System.out.println("Welcome Madhupriya");
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Write Something");
+	String s=sc.nextLine();
+	
+}
+}
