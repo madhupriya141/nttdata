a7370069b783e6e5adeb96d04e3b58274224ad80
madhupriya141 <62684063+madhupriya141@users.noreply.github.com>
2020-03-26 17:11:49 +0530

    Update task.java

--- assignment1/src/com/assignment/task.java
+++ assignment1/src/com/assignment/task.java
@@ -8,6 +8,7 @@ public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
 	System.out.println("Write Something");
 	String s=sc.nextLine();
+	System.out.println("GIT TUTORIAL");
 	
 }
 }
